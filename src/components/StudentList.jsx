70fa6b7d7001a94ecd2e713148e7e31b085a6a07
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-08-22 21:32:12 -0500

    rough CSS to display all students in array

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -4,20 +4,34 @@ import { Link } from "react-router-dom";
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 export const StudentList = () => {
-  const [printStudents, setPrintStudents] = useState([]);
+  const [students, setStudents] = useState([]);
 
   const getStudents = () => {
     axios.get("http://localhost:8080/students").then((response) => {
-      console.log(response);
-      setPrintStudents(response.data);
+      console.log(response.data);
+      setStudents(response.data);
     });
   };
 
   return (
     <div>
+      <div>
+        <h1>All Students</h1>
+        <h3>
+          <span className="p3">First Name</span>
+          <span className="p4">Last Name</span>
+          <span className="p5">Grade</span>
+        </h3>
+        {students.map((s) => (
+          <p key={s.id}>
+            <span className="p1">{s.firstName}</span>
+            <span>{s.lastName}</span>
+            <span className="p2">{s.grade}</span>
+          </p>
+        ))}
+      </div>
       <ListGroup className="mt-4">
         <ListGroupItem className="d-flex">
-          <p>Student One</p>
           <div className="m1-auto">
             <Link className="btn btn-warning mr-5" to="update/1">
               Update
