36a9dd718d6624e10ac15b5dfca248e925b318c7
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-08-22 22:41:20 -0500

    Create student works

--- src/pages/CreateStudent.jsx
+++ src/pages/CreateStudent.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 export const CreateStudent = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [grade, setGrade] = useState("");
+
+  const addStudent = (e) => {
+    e.preventDefault();
+    console.log(firstName);
+    console.log(lastName);
+    console.log(grade);
+    const postStudent = {
+      firstName,
+      lastName,
+      grade,
+    };
+    axios.post("http://localhost:8080/save", postStudent).then((response) => {
+      console.log(response);
+    });
+  };
+
   return (
     <div>
       <h1>Create a New Student</h1>
@@ -10,9 +30,27 @@ export const CreateStudent = () => {
       <Form>
         <FormGroup>
           <Label>First Name</Label>
-          <Input type="text" placeholder="Enter Student First Name"></Input>
+          <Input
+            type="text"
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="Student First Name"
+          ></Input>
+          <Label>Last Name</Label>
+          <Input
+            type="text"
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Student Last Name"
+          ></Input>
+          <Label>Grade</Label>
+          <Input
+            type="text"
+            onChange={(e) => setGrade(e.target.value)}
+            placeholder="Student Grade"
+          ></Input>
         </FormGroup>
-        <Button type="submit">Submit</Button>
+        <Button type="submit" onClick={addStudent}>
+          Submit
+        </Button>
         <Link to="/" className="btn btn-danger">
           Cancel
         </Link>
